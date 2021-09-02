610425fd18ce8c1c0acd57a37b22cff4159ff186
sonam1812 <sonampal832@gmail.com>
2021-09-02 19:08:28 +0530

    Welcome To Address Book System

---
+++ main/java/com/bridgelabz/AddressBookSystem/AddressBookSystem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.AddressBookSystem;
+
+public class AddressBookSystem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book .....");
+	}
+
+}
---
+++ main/java/com/bridgelabz/AddressBookSystem/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.AddressBookSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
